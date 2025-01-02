ec98fbad1b7f534092011b4dbf36358f8f2ff1ef
devhehexd <rlqja72201@gmail.com>
2025-01-02 19:10:28 +0900

    1/2 강사 마이페이지 작업 진행 중

--- src/infrastructure/api/endpoints.js
+++ src/infrastructure/api/endpoints.js
@@ -127,7 +127,9 @@ export const API_ENDPOINTS = {
   },
   STATS: {
     INSTRUCTOR: `${BACKEND_URL}/instructor/stats`,
-    MANUAL_UPDATE: `${BACKEND_URL}/instructor/stats/manual-update`
+    MANUAL_UPDATE: `${BACKEND_URL}/instructor/stats/manual-update`,
+    RECENT_ENROLLMENTS: `${BACKEND_URL}/courses/instructor/me/recentenrollments`,
+    RECENT_REVIEWS: `${BACKEND_URL}/courses/instructor/me/recentreviews`
   },
 };
 
